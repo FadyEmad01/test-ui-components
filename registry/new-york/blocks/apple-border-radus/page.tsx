8f886098e91332045678ea7e02bf9c9068568dfc
fady emad <fadyemad933@gmail.com>
2025-04-27 18:46:58 +0300

    v2

--- registry/new-york/blocks/apple-border-radus/page.tsx
+++ registry/new-york/blocks/apple-border-radus/page.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Squircle from './components/Squircle'
-import { Music4 } from 'lucide-react'
+import React from "react";
+import Squircle from "./components/Squircle";
+import { Music4 } from "lucide-react";
 
 const Squircles = () => {
   return (
-    <div className='w-full h-full justify-center flex'>
-        <Squircle>
-            <div className="aspect-square w-[200px] flex justify-center items-center bg-linear-to-t from-[#FA233B] to-[#FB5C74]">
-            <Music4  className='w-[2100px] h-[100px] text-white'/>
-            </div>
-        </Squircle>
+    <div className="w-full flex justify-center items-center">
+      <Squircle>
+        <div className="aspect-square w-[200px] flex justify-center items-center bg-linear-to-t from-[#FA233B] to-[#FB5C74]">
+          <Music4 className="w-[100px] h-[100px] text-white" />
+        </div>
+      </Squircle>
     </div>
-  )
-}
+  );
+};
 
-export default Squircles
+export default Squircles;
